e0e949fb9d9a42b7ef824a5471198774a3871be3
Fagner JB <para@fagnerjb.com>
2025-10-08 03:55:40 -0300

    ✨ [Editor] Lista de atalhos

---
+++ cavwp/wp-content/themes/writers-camp/pages/page-publish/components/shortcuts.php
@@ -0,0 +1,40 @@
+<dialog id="shortcuts" class="m-auto w-full max-w-md rounded-lg backdrop:z-45 backdrop:bg-neutral-900/60 bg-yellow-100 text-neutral-700 dark:text-neutral-100 dark:bg-neutral-700" x-on:click.self="document.getElementById('shortcuts').close()">
+   <div class="py-3 px-4" x-data="{parseKeys: (input) => input.split('+').map(key => `<kbd>${key}</kbd>`).join('+')}">
+      <div class="flex justify-between items-center mb-6">
+         <h2 class="h2">Teclas de atalho</h2>
+         <button class="text-xl py-1 px-2 cursor-pointer" title="Fechar" type="button" x-on:click.prevent="document.getElementById('shortcuts').close()">
+            <i class="ri-close-line"></i>
+         </button>
+      </div>
+      <ul class="flex flex-col gap-0.75 *:flex *:justify-between">
+         <li>
+            Salvar rascunho <span class="flex gap-0.5"><kbd>Ctrl</kbd>+<kbd>S</kbd></span>
+         </li>
+         <li>
+            <hr class="border-neutral-600 dark:border-neutral-200 py-0.25" />
+         </li>
+         <template x-for="{label, shortcut} in sky.align">
+            <li>
+               <span x-text="label"></span> <span class="flex gap-0.5" x-html="parseKeys(shortcut)"></span>
+            </li>
+         </template>
+         <template x-for="{label, shortcut} in sky.blocks">
+            <template x-if="!!shortcut">
+               <li>
+                  <span x-text="label"></span> <span class="flex gap-0.25" x-html="parseKeys(shortcut)"></span>
+               </li>
+            </template>
+         </template>
+         <li>
+            <hr class="border-neutral-600 dark:border-neutral-200 py-0.25" />
+         </li>
+         <template x-for="{label, shortcut} in sky.marks">
+            <template x-if="!!shortcut">
+               <li>
+                  <span x-text="label"></span> <span class="flex gap-0.5" x-html="parseKeys(shortcut)"></span>
+               </li>
+            </template>
+         </template>
+      </ul>
+   </div>
+</dialog>
--- cavwp/wp-content/themes/writers-camp/pages/page-publish/components/status.php
+++ cavwp/wp-content/themes/writers-camp/pages/page-publish/components/status.php
@@ -1,3 +1,6 @@
-<ul class="hidden lg:flex gap-3 text-sm">
+<ul class="flex gap-3 text-sm">
    <li><span x-text="current.words"></span> palavra<span x-text="current.words === 1 ? '' : 's'"></span></li>
+   <li><button class="cursor-pointer" type="button" x-on:click.prevent="document.getElementById('shortcuts').showModal()">
+         <i class="ri-information-2-fill"></i> Teclas de atalho
+      </button></li>
 </ul>
